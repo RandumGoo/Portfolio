90dada3429c3cd174bfa7076f8ffde10afd970e3
RandumGoo <166697600+RandumGoo@users.noreply.github.com>
2024-11-25 16:57:15 +0100

    Ajout de l'app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Components/Header/Header';
+import Nav from './Components/Nav/Nav';
+import Languages from './Components/Languages/Languages';
+import Projects from './Components/Projects/Projects';
+import Contact from './Components/Contact/Contact';
+import './Components/main.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <div className="content">
+        <section id="About"><Header /></section>
+        <section id="languages-used"><Languages /></section>
+        <section id="projects"><Projects /></section>
+        <section id="contact"><Contact /></section>
+      </div>
     </div>
   );
 }
